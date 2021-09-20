e33c4225902c0e496863bb1ebc9bad45ec1f5570
Adam Chew <adamchewys@gmail.com>
2021-09-20 11:22:53 -0400

    replace backend dir with submodule

--- backend/src/middleware/index.js
+++
@@ -1,9 +0,0 @@
-import models from '../models';
-
-const httpsRedirect = (req, res, next) => {
-  if (req.header('x-forwarded-proto') !== 'https')
-    return res.redirect(`https://${req.headers.host}${req.url}`);
-  next();
-};
-
-export { httpsRedirect };
